1f0a60ea1e3bea075ecee10adef1ed89e25fae62
Aswita <aswitajohn@gmail.com>
2023-07-27 18:30:42 +0530

    Level 3 of Authentication & Security - Hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -16,17 +16,12 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:['password']});
-
 const User = new mongoose.model("User",userSchema);
-
 
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/userDB");
     console.log("Connected to userDB");
 }
-
-
 
 app.get("/",function(req,res){
     res.render("home");
@@ -43,7 +38,7 @@ app.get("/register",function(req,res){
 app.post("/register",async function(req,res){
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     await newUser.save();
     res.render("secrets");
@@ -51,7 +46,7 @@ app.post("/register",async function(req,res){
 
 app.post("/login",async function (req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     const foundUser = await User.findOne({email:username});
     if(foundUser){
         console.log("Found!!!");
